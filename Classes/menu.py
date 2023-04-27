be72f79f6453a0dfb0de0f984a8b9e56fda4fbac
aiv214 <andrey.ivashko@khpi.edu.ua>
2023-04-27 10:59:49 +0300

    Optimize stage

--- Classes/menu.py
+++ Classes/menu.py
@@ -1,57 +1,71 @@
 from Classes.tasks import Tasks
 
+# --------------------------Menu configuration constants----------------------------------
+ITEMS_OF_MENU = (
+    "1. Get all titles of series/movies etc. that are available in Ukrainian",
+    "2. Get the list of people’s names, who were born in the 19th century",
+    "3. Get titles of all movies that last more than 2 hours",
+    "4. Get names of people, corresponding movies/series and characters they played in those films",
+    ("5. Get information about how many adult movies/series etc. there are per"
+     "region. Get the top 100 of them from the region with the biggest count to"
+     "the region with the smallest one"),
+    ("6. Get information about how many episodes in each TV Series.Get the top"
+     "50 of them starting from the TV Series with the biggest quantity of episodes"),
+    "7. Get 10 titles of the most popular movies/series etc. by each decade",
+    "8. Get 10 titles of the most popular movies/series etc. by each genre",
+    "0. Exit"
+)
+INCORRECT_INPUT_TO_CHOICE = "Incorrect input !!!"
+BY_BY = "By-by !!!"
+INCORRECT_NUMBER_OF_ITEM = "Incorrect number of item !!!"
+WAITING_MESSAGE = "Wait!!! I'm thinking ..."
+# --------------------------------------------------------------------------------------------
+
 
 class Menu:
     def __init__(self):
-        self.items = (
-            "1. Get all titles of series/movies etc. that are available in Ukrainian",
-            "2. Get the list of people’s names, who were born in the 19th century",
-            "3. Get titles of all movies that last more than 2 hours",
-            "4. Get names of people, corresponding movies/series and characters they played in those films",
-            ("5. Get information about how many adult movies/series etc. there are per"
-             "region. Get the top 100 of them from the region with the biggest count to"
-             "the region with the smallest one"),
-            ("6. Get information about how many episodes in each TV Series.Get the top"
-             "50 of them starting from the TV Series with the biggest quantity of episodes"),
-            "7. Get 10 titles of the most popular movies/series etc. by each decade",
-            "8. Get 10 titles of the most popular movies/series etc. by each genre",
-            "0. Exit"
-        )
+        self.items = ITEMS_OF_MENU
+        self.incorrect_input = INCORRECT_INPUT_TO_CHOICE
+        self.by_by = BY_BY
+        self.incorrect_item = INCORRECT_NUMBER_OF_ITEM
+        self.wait = WAITING_MESSAGE
+        print(self.wait)
+        self.tasks = Tasks()
 
     def out_menu(self):
         sel_item = -1
-        while sel_item != 0 :
-            print("---------------------------Task menu------------------------------------")
+        while sel_item != 0:
+            print("---------------------------------Task menu--------------------------------------------")
             for item in self.items:
                 print(item, end="\n")
-            print("------------------------------------------------------------------------")
-            sel_item = input("Choice item:")
+            print("---------------------------------------------------------------------------------------")
+            sel_item = input("Choice number of item:")
             if not sel_item.isnumeric():
                 sel_item = -1
-                print("Incorrect input!!!")
+                print(self.incorrect_input)
             else:
-                sel_item=int(sel_item)
+                sel_item = int(sel_item)
                 self.switch_item(sel_item)
 
     def switch_item(self, item):
-        tasks = Tasks()
+        print(self.wait)
         if item == 1:
-            tasks.show_task1()
+            self.tasks.show_task1()
         elif item == 2:
-            tasks.show_task2()
+            self.tasks.show_task2()
         elif item == 3:
-            tasks.show_task3()
+            self.tasks.show_task3()
         elif item == 4:
-            tasks.show_task4()
+            self.tasks.show_task4()
         elif item == 5:
-            tasks.show_task5()
+            self.tasks.show_task5()
         elif item == 6:
-            tasks.show_task6()
+            self.tasks.show_task6()
         elif item == 7:
-            tasks.show_task7()
+            self.tasks.show_task7()
         elif item == 8:
-            tasks.show_task8()
+            self.tasks.show_task8()
         elif item == 0:
-            print("By-by!!!")
+            print(self.by_by)
         else:
-            print("Incorrect number of item")
+            print(self.incorrect_item)
--- Classes/tasks.py
+++ Classes/tasks.py
@@ -1,4 +1,5 @@
 import findspark
+from pip import __main__
 from pyspark import SparkConf
 from pyspark.sql import SparkSession, Window
 import pyspark.sql.types as t
@@ -6,17 +7,26 @@ import pyspark.sql.functions as f
 
 findspark.init()
 
+# ----------------Constants for configuring project tasks-------------------------------------
+COUNT_TO_SHOW = 20
 TOP_COUNT_REGIONS = 100
 
 TOP_COUNT_EPISODES = 50
 
 TOP_RATING_COUNT = 10
 COUNT_GROUP_VIEW = 5
+# -------------------------Paths to source files-----------------------------------------------
+PATH_TITLE_AKAS_TSV = "Data/title_akas.tsv"
+PATH_NAME_BASICS_TSV = "Data/name_basics.tsv"
+PATH_TITLE_BASICS_TSV = "Data/title_basics.tsv"
+PATH_TITLE_PRINCIPALS_TSV = "Data/title_principals.tsv"
+PATH_TITLE_EPISODE_TSV = "Data/title_episode.tsv"
+PATH_TITLE_RATINGS_TSV = "Data/title_ratings.tsv"
+# ---------------------------------------------------------------------------------------------
 
 
 class Tasks:
     def __init__(self):
-
         self.spark_session = (SparkSession.builder
                               .master("local")
                               .appName("task app")
@@ -65,36 +75,40 @@ class Tasks:
                                                       t.StructField("averageRating", t.DoubleType(), False),
                                                       t.StructField("numVotes", t.IntegerType(), False)])
 
+    # 1. Get all titles of series/movies etc. that are available in Ukrainian
     def show_task1(self):
-        df_task1 = self.spark_session.read.csv("Data/title_akas.tsv", schema=self.schema_tsv_title_akas,
+        df_task1 = self.spark_session.read.csv(PATH_TITLE_AKAS_TSV, schema=self.schema_tsv_title_akas,
                                                header=True, sep="\t")
         df_task1 = df_task1.select("title", "region").where(f.col("region") == "UA")
-        df_task1.show()
+        df_task1.show(COUNT_TO_SHOW)
         # print("I'm writing ...")
         # df_task1.write.csv("Results/task1")
 
+    # 2. Get the list of people’s names, who were born in the 19th century
     def show_task2(self):
-        df_task2 = self.spark_session.read.csv("Data/name_basics.tsv", schema=self.schema_tsv_name_basics,
+        df_task2 = self.spark_session.read.csv(PATH_NAME_BASICS_TSV, schema=self.schema_tsv_name_basics,
                                                header=True, sep="\t")
         df_task2 = df_task2.select("primaryName", "birthYear")
         df_task2 = df_task2.na.fill(0)
         df_task2 = df_task2.where((f.col("birthYear") >= 1800) & (f.col("birthYear") < 1900))
-        df_task2.show()
+        df_task2.show(COUNT_TO_SHOW)
 
+    # 3. Get titles of all movies that last more than 2 hours
     def show_task3(self):
-        df_task3 = self.spark_session.read.csv("Data/title_basics.tsv", schema=self.schema_tsv_title_basics,
+        df_task3 = self.spark_session.read.csv(PATH_TITLE_BASICS_TSV, schema=self.schema_tsv_title_basics,
                                                header=True, sep="\t")
         df_task3 = df_task3.select("primaryTitle", "titleType", "runtimeMinutes")
         df_task3 = df_task3.na.fill(0)
         df_task3 = df_task3.where((f.col("runtimeMinutes") > 120) & (f.col("titleType") == "movie"))
-        df_task3.show()
+        df_task3.show(COUNT_TO_SHOW)
 
+    # 4. Get names of people, corresponding movies/series and characters they played in those films
     def show_task4(self):
-        df_actors = self.spark_session.read.csv("Data/name_basics.tsv", schema=self.schema_tsv_name_basics,
+        df_actors = self.spark_session.read.csv(PATH_NAME_BASICS_TSV, schema=self.schema_tsv_name_basics,
                                                 header=True, sep="\t")
-        df_films = self.spark_session.read.csv("Data/title_basics.tsv", schema=self.schema_tsv_title_basics,
+        df_films = self.spark_session.read.csv(PATH_TITLE_BASICS_TSV, schema=self.schema_tsv_title_basics,
                                                header=True, sep="\t")
-        df_actors_films = self.spark_session.read.csv("Data/title_principals.tsv",
+        df_actors_films = self.spark_session.read.csv(PATH_TITLE_PRINCIPALS_TSV,
                                                       schema=self.schema_tsv_title_principals, header=True, sep="\t")
         df_actors = df_actors.select("primaryName", "nconst")
         df_actors_films = df_actors_films.drop("ordering", "job")
@@ -103,10 +117,12 @@ class Tasks:
         df_actors = df_actors.filter(df_actors.category == "actor")
         df_actors = df_actors.join(df_films, df_actors.tconst == df_films.tconst, "inner")
         df_actors = df_actors.drop("nconst", "tconst")
-        df_actors.show()
+        df_actors.show(COUNT_TO_SHOW)
 
+    # 5. Get information about how many adult movies/series etc. there are perregion. Get the top 100 of them from the
+    # region with the biggest count tithe region with the smallest one
     def show_task5(self):
-        df_task5 = self.spark_session.read.csv("Data/title_akas.tsv", schema=self.schema_tsv_title_akas,
+        df_task5 = self.spark_session.read.csv(PATH_TITLE_AKAS_TSV, schema=self.schema_tsv_title_akas,
                                                header=True, sep="\t")
         df_task5 = df_task5.select("title", "region")
         df_task5 = df_task5.groupBy("region").count()
@@ -117,10 +133,12 @@ class Tasks:
         print("Top 100 of them from the region with the biggest count")
         df_task5.show(TOP_COUNT_REGIONS)
 
+    # 6. Get information about how many episodes in each TV Series.Get the top50 of them starting from the TV Series
+    # with the biggest quantity of episodes
     def show_task6(self):
-        df_films = self.spark_session.read.csv("Data/title_basics.tsv", schema=self.schema_tsv_title_basics,
+        df_films = self.spark_session.read.csv(PATH_TITLE_BASICS_TSV, schema=self.schema_tsv_title_basics,
                                                header=True, sep="\t")
-        df_episodes = self.spark_session.read.csv("Data/title_episode.tsv", schema=self.schema_tsv_title_episode,
+        df_episodes = self.spark_session.read.csv(PATH_TITLE_EPISODE_TSV, schema=self.schema_tsv_title_episode,
                                                   header=True, sep="\t")
         df_films = df_films.select("tconst", "primaryTitle")
         df_episodes = df_episodes.select("tconst", "episodeNumber")
@@ -128,12 +146,13 @@ class Tasks:
         df_films = df_films.drop("tconst").orderBy(f.col("episodeNumber").desc()).limit(TOP_COUNT_EPISODES)
         df_films.show(TOP_COUNT_EPISODES)
 
+    # 7. Get 10 titles of the most popular movies/series etc. by each decade
     def show_task7(self):
-        df_films = self.spark_session.read.csv("Data/title_basics.tsv", schema=self.schema_tsv_title_basics,
+        df_films = self.spark_session.read.csv(PATH_TITLE_BASICS_TSV, schema=self.schema_tsv_title_basics,
                                                header=True, sep="\t")
         df_films = df_films.na.fill(0).filter((f.col("startYear") != 0) & (f.col("endYear") != 0))
         df_films = df_films.withColumn("decade", ((df_films.endYear - df_films.startYear) / 10 + 1).cast("int"))
-        df_ratings = self.spark_session.read.csv("Data/title_ratings.tsv", schema=self.schema_tsv_title_ratings,
+        df_ratings = self.spark_session.read.csv(PATH_TITLE_RATINGS_TSV, schema=self.schema_tsv_title_ratings,
                                                  header=True, sep="\t")
         df_films = df_films.join(df_ratings, df_films.tconst == df_ratings.tconst, "inner")
         df_films = df_films.select("primaryTitle", "decade", "averageRating")
@@ -142,10 +161,11 @@ class Tasks:
         df_films = df_films.filter(f.col("top") <= TOP_RATING_COUNT)
         df_films.show(COUNT_GROUP_VIEW * TOP_RATING_COUNT)
 
+    # 8. Get 10 titles of the most popular movies/series etc. by each genre
     def show_task8(self):
-        df_films = self.spark_session.read.csv("Data/title_basics.tsv", schema=self.schema_tsv_title_basics,
+        df_films = self.spark_session.read.csv(PATH_TITLE_BASICS_TSV, schema=self.schema_tsv_title_basics,
                                                header=True, sep="\t")
-        df_ratings = self.spark_session.read.csv("Data/title_ratings.tsv", schema=self.schema_tsv_title_ratings,
+        df_ratings = self.spark_session.read.csv(PATH_TITLE_RATINGS_TSV, schema=self.schema_tsv_title_ratings,
                                                  header=True, sep="\t")
         df_films = df_films.join(df_ratings, df_films.tconst == df_ratings.tconst, "inner")
         df_films = df_films.select("primaryTitle", "genres", "averageRating")
@@ -154,3 +174,16 @@ class Tasks:
         df_films = df_films.withColumn("top", f.row_number().over(window_dept))
         df_films = df_films.filter(f.col("top") <= TOP_RATING_COUNT)
         df_films.show(COUNT_GROUP_VIEW * TOP_RATING_COUNT)
+
+
+# To test the operation of methods of this class, uncomment the desired task and run it
+if __name__ == __main__:
+    tasks = Tasks()
+    # tasks.show_task1()
+    # tasks.show_task2()
+    # tasks.show_task3()
+    # tasks.show_task4()
+    # tasks.show_task5()
+    # tasks.show_task6()
+    # tasks.show_task7()
+    # tasks.show_task8()
