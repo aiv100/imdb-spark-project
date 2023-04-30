883f074e4aa80f2bf58fa853502c4cc18141f46b
aiv214 <andrey.ivashko@khpi.edu.ua>
2023-04-30 15:46:16 +0300

    Add write CSV files stage

--- Classes/tasks.py
+++ Classes/tasks.py
@@ -15,6 +15,9 @@ TOP_COUNT_EPISODES = 50
 
 TOP_RATING_COUNT = 10
 COUNT_GROUP_VIEW = 5
+
+WRITE_QUESTION = "Is it necessary to create a CSV file ('Y' or 'y'- yes, anything - no)?"
+WRITE_MESSAGE = "I'm writing ..."
 # -------------------------Paths to source files-----------------------------------------------
 PATH_TITLE_AKAS_TSV = "Data/title.akas.tsv"
 PATH_NAME_BASICS_TSV = "Data/name.basics.tsv"
@@ -22,6 +25,8 @@ PATH_TITLE_BASICS_TSV = "Data/title.basics.tsv"
 PATH_TITLE_PRINCIPALS_TSV = "Data/title.principals.tsv"
 PATH_TITLE_EPISODE_TSV = "Data/title.episode.tsv"
 PATH_TITLE_RATINGS_TSV = "Data/title.ratings.tsv"
+# -------------------------Path to result files-----------------------------------------------
+PATH_CSV_TASKS = "Results/task"
 # ---------------------------------------------------------------------------------------------
 
 
@@ -75,14 +80,16 @@ class Tasks:
                                                       t.StructField("averageRating", t.DoubleType(), False),
                                                       t.StructField("numVotes", t.IntegerType(), False)])
 
+        self.write_question =  WRITE_QUESTION
+        self.write_message = WRITE_MESSAGE
+
     # 1. Get all titles of series/movies etc. that are available in Ukrainian
     def show_task1(self):
         df_task1 = self.spark_session.read.csv(PATH_TITLE_AKAS_TSV, schema=self.schema_tsv_title_akas,
                                                header=True, sep="\t")
         df_task1 = df_task1.select("title", "region").where(f.col("region") == "UA")
         df_task1.show(COUNT_TO_SHOW)
-        # print("I'm writing ...")
-        # df_task1.write.csv("Results/task1")
+        self.write_csv_file(df_task1, 1)
 
     # 2. Get the list of people’s names, who were born in the 19th century
     def show_task2(self):
@@ -92,6 +99,7 @@ class Tasks:
         df_task2 = df_task2.na.fill(0)
         df_task2 = df_task2.where((f.col("birthYear") >= 1800) & (f.col("birthYear") < 1900))
         df_task2.show(COUNT_TO_SHOW)
+        self.write_csv_file(df_task2, 2)
 
     # 3. Get titles of all movies that last more than 2 hours
     def show_task3(self):
@@ -101,6 +109,7 @@ class Tasks:
         df_task3 = df_task3.na.fill(0)
         df_task3 = df_task3.where((f.col("runtimeMinutes") > 120) & (f.col("titleType") == "movie"))
         df_task3.show(COUNT_TO_SHOW)
+        self.write_csv_file(df_task3, 3)
 
     # 4. Get names of people, corresponding movies/series and characters they played in those films
     def show_task4(self):
@@ -118,8 +127,9 @@ class Tasks:
         df_actors = df_actors.join(df_films, df_actors.tconst == df_films.tconst, "inner")
         df_actors = df_actors.drop("nconst", "tconst")
         df_actors.show(COUNT_TO_SHOW)
+        self.write_csv_file(df_actors, 4)
 
-    # 5. Get information about how many adult movies/series etc. there are perregion. Get the top 100 of them from the
+    # 5. Get information about how many adult movies/series etc. there are per region. Get the top 100 of them from the
     # region with the biggest count tithe region with the smallest one
     def show_task5(self):
         df_task5 = self.spark_session.read.csv(PATH_TITLE_AKAS_TSV, schema=self.schema_tsv_title_akas,
@@ -132,6 +142,7 @@ class Tasks:
         df_task5 = df_task5.orderBy(f.col("count").desc()).limit(TOP_COUNT_REGIONS)
         print("Top 100 of them from the region with the biggest count")
         df_task5.show(TOP_COUNT_REGIONS)
+        self.write_csv_file(df_task5, 5)
 
     # 6. Get information about how many episodes in each TV Series.Get the top50 of them starting from the TV Series
     # with the biggest quantity of episodes
@@ -145,6 +156,7 @@ class Tasks:
         df_films = df_films.join(df_episodes, df_films.tconst == df_episodes.tconst, "inner")
         df_films = df_films.drop("tconst").orderBy(f.col("episodeNumber").desc()).limit(TOP_COUNT_EPISODES)
         df_films.show(TOP_COUNT_EPISODES)
+        self.write_csv_file(df_films, 6)
 
     # 7. Get 10 titles of the most popular movies/series etc. by each decade
     def show_task7(self):
@@ -160,6 +172,7 @@ class Tasks:
         df_films = df_films.withColumn("top", f.row_number().over(window_dept))
         df_films = df_films.filter(f.col("top") <= TOP_RATING_COUNT)
         df_films.show(COUNT_GROUP_VIEW * TOP_RATING_COUNT)
+        self.write_csv_file(df_films, 7)
 
     # 8. Get 10 titles of the most popular movies/series etc. by each genre
     def show_task8(self):
@@ -174,6 +187,13 @@ class Tasks:
         df_films = df_films.withColumn("top", f.row_number().over(window_dept))
         df_films = df_films.filter(f.col("top") <= TOP_RATING_COUNT)
         df_films.show(COUNT_GROUP_VIEW * TOP_RATING_COUNT)
+        self.write_csv_file(df_films, 8)
+
+    def write_csv_file(self, df, num_task):
+        ans=input(self.write_question)
+        if ans == "Y" or ans == "y":
+            print(self.write_message)
+            df.write.csv(PATH_CSV_TASKS+str(num_task))
 
 
 # To test the operation of methods of this class, uncomment the desired task and run it
